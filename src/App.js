95faa9d60e4856f14c36d310f74664cec3c2c69c
Antonio Cruz <antonio.cruz5612@gmail.com>
2020-11-23 16:27:07 -0800

    add Navbar and Nav imports.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, 
 Route, 
 Link } from 'react-router-dom';
+import { Container as Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from'react-bootstrap/Nav';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import './App.css';
 
 class App extends React.Component {
@@ -32,7 +36,18 @@ class App extends React.Component {
   render () {
     return (
       <Router>
-        
+        <Container className="p-1" fluid={true}>
+          <Navbar className="border-bottom">
+            <Navbar.Brand>Antonio Cruz</Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav>
+                <Link className="nav-link">Home</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
       </Router>
     );
   }
