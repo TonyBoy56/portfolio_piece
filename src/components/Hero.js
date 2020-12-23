5542714b5f5cabbc55d730295bb405547ff37691
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-12-23 11:48:15 -0800

    passing props to Hero content.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function Hero() {
+function Hero(props) {
     return (
-        <p>Hero Works!</p>
+        <Jumbotron>
+            <Container>
+                <Row>
+                    <Col>
+                        { props.title &&  <h1>{props.title}</h1>}
+                        { props.subTitle &&  <h1>{props.subTitle}</h1>}
+                        { props.text &&  <h1>{props.text}</h1>}
+                    </Col>
+                </Row>
+            </Container>
+        </Jumbotron>
     );
 }
 
