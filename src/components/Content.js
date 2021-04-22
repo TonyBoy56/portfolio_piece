50131678411a05e2dd7c928a21f01a4ba6a60d5b
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-04-22 11:50:20 -0700

    add Content.js component to the project.

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+function Content(props) {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Content;
